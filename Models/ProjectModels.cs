c2e6707473b565b0e069943d99ad1cf72cdcfa46
pahachic <pahachic3@gmail.com>
2025-05-29 17:58:18 +0500

    project creation

---
+++ Models/ProjectModels.cs
@@ -0,0 +1,15 @@
+namespace BlazorTaskTracker.Models
+{
+    public class Project
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Color { get; set; } = "bg-blue-500"; // Цвет для идентификации проекта
+        public string Icon { get; set; } = "📋"; // Эмодзи иконка для проекта
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public List<string> Members { get; set; } = new();
+        public int TaskCount { get; set; } = 0;
+        public bool IsActive { get; set; } = true;
+    }
+} 
